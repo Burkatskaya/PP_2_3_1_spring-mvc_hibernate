68907b784baccc69f50fd649a96eb38ae1d0d693
Burkatskaya <anyaburkatskaya@gmail.com>
2024-01-09 17:20:51 +0300

    Create controller - UserController.java

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,22 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/users")
+    public String showUsers(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
+        model.addAttribute("name", "Users:");
+        model.addAttribute("users", userService.listUsersByCount(count));
+        return "users";
+    }
+}
