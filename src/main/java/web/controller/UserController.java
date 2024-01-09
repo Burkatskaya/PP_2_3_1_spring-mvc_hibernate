396e5fa37cf46882c17498a266bb0ba12320856a
Burkatskaya <anyaburkatskaya@gmail.com>
2024-01-09 17:43:47 +0300

    UserController.java has been modified

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import web.service.UserService;
+
 @Controller
 public class UserController {
     private final UserService userService;
