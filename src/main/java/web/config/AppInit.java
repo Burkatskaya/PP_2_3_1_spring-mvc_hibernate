5d1fb90a6a2a4d365e06c3ee0841649f7611c37a
Burkatskaya <anyaburkatskaya@gmail.com>
2024-01-19 15:02:19 +0300

    AppInit.java has been changed

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -1,38 +1,24 @@
 package web.config;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class<?>[]{
-            AppConfig.class
+                AppConfig.class
         };
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{
-            WebConfig.class
+                WebConfig.class
         };
     }
 
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
-    }
-
-    @Override
-    public void onStartup(ServletContext aServletContext) throws ServletException {
-        super.onStartup(aServletContext);
-        registerHiddenFieldFilter(aServletContext);
-    }
-
-    private void registerHiddenFieldFilter(ServletContext aContext) {
-        aContext.addFilter("hiddenHttpMethodFilter",
-                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
     }
 }
