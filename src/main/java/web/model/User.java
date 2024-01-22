98af8b6d7e8df65694ab1874ff8c0bd98b31aa9d
Burkatskaya <anyaburkatskaya@gmail.com>
2024-01-22 13:38:10 +0300

    equals() and hashCode() has been added

--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -7,6 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.Objects;
+
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -50,5 +53,22 @@ public class User {
 
     public void setLastname(String lastname) {
         this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id && Objects.equals(name, user.name) && Objects.equals(lastname, user.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastname);
     }
 }
